db35101444bdccf992873a057a7019d8adf11cbe
Ravindu Fernando <45675690+ravindukrs@users.noreply.github.com>
2026-06-12 00:43:35 +0530

    instance type

--- terraform-demo/02-infrastructure/main.tf
+++ terraform-demo/02-infrastructure/main.tf
@@ -150,7 +150,7 @@ resource "aws_security_group" "web_server" {
 # EC2 Instance
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.amazon_linux_2023.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.web_server.id]
---
+++ terraform-demo/02-infrastructure/variables.tf
@@ -0,0 +1,4 @@
+variable "instance_type" {
+    type    = string
+    value   = "t3.small"
+}
